61474e6c5a51883f18706a69cf10c3e7a0f6e33a
ahmed magdy <66494182+EngAhmedMagdy@users.noreply.github.com>
2023-03-08 20:23:45 +0200

    Add files via upload

---
+++ CreationalDesignPatterns/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciples.CreationalDesignPatterns
+{
+    internal class Singleton
+    {
+        //make it class method
+        private static Singleton _UniqueInstance;
+        private Singleton()
+        {
+
+        }
+        public static Singleton getInstance()
+        {
+            if(_UniqueInstance == null)
+            {
+                _UniqueInstance = new Singleton();
+            }
+            return _UniqueInstance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesignPrinciples.CreationalDesignPatterns;
+
+Singleton singleton = new Singleton();
+var instance1 = singleton.getInstance();
+var instance2 = singleton.getInstance();
+
+if (instance1 == instance2)
+{
+    Console.WriteLine("same instance");
+}
+else
+{
+    Console.WriteLine("diff instance");
+}
